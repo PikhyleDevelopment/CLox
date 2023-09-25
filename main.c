e20de6b44222fa5b8164ffccf7c20b28b28fd0cf
Erik Mason <info@techlabllc.net>
2023-09-25 17:49:37 -0400

    Basic instruction set and debugging so we can see the program in action

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
+	// Global bytecode chunk
+	Chunk chunk;
+	// Initialize the chunk
+	initChunk(&chunk);
+
+	int constant = addConstant(&chunk, 1.2);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_RETURN, 123);
+
+	/* TESTING ONLY */
+	disassembleChunk(&chunk, "test chunk");
+	// Cleanup
+	freeChunk(&chunk);
 	return 0;
 }
