82c2d8734fdd75241805900c25a3cd8a66b674bf
Erik Mason <info@techlabllc.net>
2023-09-26 10:01:03 -0400

    Introduces the VM's stack. Hand-authored chunk shows stack operation.

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
@@ -10,11 +14,43 @@ int main(int argc, const char* argv[]) {
 	// Initialize the chunk
 	initChunk(&chunk);
 
-	int constant = addConstant(&chunk, 1.2);
+	/*int constant = addConstant(&chunk, 1.0);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);*/
+
+	int constant = addConstant(&chunk, 1.0);
 	writeChunk(&chunk, OP_CONSTANT, 123);
 	writeChunk(&chunk, constant, 123);
 
+	constant = addConstant(&chunk, 2.0);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	constant = addConstant(&chunk, 3.0);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_MULTIPLY, 123);
+
+	writeChunk(&chunk, OP_ADD, 123);
+
+	constant = addConstant(&chunk, 4.0);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	constant = addConstant(&chunk, 5.0);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+
+	writeChunk(&chunk, OP_NEGATE, 123);
+
+	writeChunk(&chunk, OP_DIVIDE, 123);
+
+	writeChunk(&chunk, OP_SUBTRACT, 123);
+
 	writeChunk(&chunk, OP_RETURN, 123);
+
+
 
 	/* TESTING ONLY */
 	disassembleChunk(&chunk, "test chunk");
