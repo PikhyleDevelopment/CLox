5eaa36a2f8ff9f65e6e5ce02df689f6befdbc0ea
Erik Mason <info@techlabllc.net>
2023-09-29 16:07:29 -0400

    Global Variables and Expressions. The interpreter and compiler now recognize print statements, variable declaration, initialization, and assignment.

--- value.c
+++ value.c
@@ -43,7 +43,7 @@ bool valuesEqual(Value a, Value b) {
 
 	switch (a.type) {
 	case VAL_BOOL:		return AS_BOOL(a) == AS_BOOL(b);
-	case VAL_NIL:		return true;
+	case VAL_NIL:		return AS_OBJ(a) == AS_OBJ(b);
 	case VAL_NUMBER:	return AS_NUMBER(a) == AS_NUMBER(b);
 	case VAL_OBJ: {
 		ObjString* aString = AS_STRING(a);
