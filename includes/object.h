4e56c1aba577d6e2cd50ee48b07a6e63a1c414d4
Erik Mason <erik@pikhyle.net>
2023-10-03 22:55:30 -0400

    Beginning work on closure.

--- includes/object.h
+++ includes/object.h
@@ -7,59 +7,69 @@
 
 #define OBJ_TYPE(value)            (AS_OBJ(value)->type)
 
+#define IS_CLOSURE(value)       isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value)      isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value)        isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value)        isObjType(value, OBJ_STRING)
 
-#define AS_FUNCTION(value)      ((ObjFunction *)AS_OBJ(value))
-#define AS_NATIVE(value)        (((ObjNative *)AS_OBJ(value))->function)
+#define AS_CLOSURE(value)       ((ObjClosure*)AS_OBJ(value))
+#define AS_FUNCTION(value)      ((ObjFunction*)AS_OBJ(value))
+#define AS_NATIVE(value)        (((ObjNative*)AS_OBJ(value))->function)
 #define AS_STRING(value)        ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)        (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
-    OBJ_FUNCTION,
-    OBJ_NATIVE,
-    OBJ_STRING,
+	OBJ_CLOSURE,
+	OBJ_FUNCTION,
+	OBJ_NATIVE,
+	OBJ_STRING,
 } ObjType;
 
 struct Obj {
-    ObjType type;
-    struct Obj *next;
+	ObjType type;
+	struct Obj* next;
 };
 
 typedef struct {
-    Obj obj;
-    int arity;
-    Chunk chunk;
-    ObjString *name;
+	Obj obj;
+	int arity;
+	Chunk chunk;
+	ObjString* name;
 } ObjFunction;
 
-typedef Value (*NativeFn)(int argCount, Value *args);
+typedef Value(*NativeFn)(int argCount, Value* args);
 
 typedef struct {
-    Obj obj;
-    NativeFn function;
+	Obj obj;
+	NativeFn function;
 } ObjNative;
 
 struct ObjString {
-    Obj obj;
-    int length;
-    char *chars;
-    uint32_t hash;
+	Obj obj;
+	int length;
+	char* chars;
+	uint32_t hash;
 };
 
-ObjFunction *newFunction();
+typedef struct {
+	Obj obj;
+	ObjFunction* function;
+} ObjClosure;
 
-ObjNative *newNative(NativeFn function);
+ObjClosure* newClosure(ObjFunction* function);
 
-ObjString *takeString(char *chars, int length);
+ObjFunction* newFunction();
 
-ObjString *copyString(const char *chars, int length);
+ObjNative* newNative(NativeFn function);
+
+ObjString* takeString(char* chars, int length);
+
+ObjString* copyString(const char* chars, int length);
 
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
-    return IS_OBJ(value) && AS_OBJ(value)->type == type;
+	return IS_OBJ(value) && AS_OBJ(value)->type == type;
 }
 
 #endif // !clox_object_h
